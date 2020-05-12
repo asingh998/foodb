c4a46d084ccb4575a425e55218c44c6136e2df7a
Arshdeep Singh <asingh69@mail.greenriver.edu>
2020-05-11 21:27:07 -0400

    added files

---
+++ model/data-layer.php
@@ -0,0 +1,16 @@
+<?php
+    //data-layer.php
+
+    /* getMeals()
+    * Returns
+    */
+
+    function getMeals() {
+        $meals = array("breakfast", "lunch", "dinner");
+        return $meals;
+    }
+
+    function getCondiments(){
+        $conds = array("mayonnaise", "ketchup", "mustard", "sriracha");
+        return $conds;
+    }
---
+++ model/validate.php
@@ -0,0 +1,40 @@
+<?php
+
+//validate.php
+
+/*
+ * validfoods
+ */
+
+function validFood($food){
+
+    /*
+    if(!empty($food) && ctype_alpha($food)) {
+        return true;
+    } else {
+        return false;
+    }
+    */
+
+    $food = str_replace(' ', '', $food);
+    return !empty($food) && ctype_alpha($food);
+}
+
+/* Return a value indicating if meal is valid
+   Valid meals are breakfast, lunch and dinner
+   @param String $meal
+   @return boolean
+*/
+function validMeal($meal)
+{
+    $meals = getMeals();
+    return in_array($meal, $meals);
+}
+
+/*
+echo validMeal('breakfast') ? "yes<br>" : "no<br>";
+echo validMeal('') ? "yes<br>" : "no<br>";
+echo validMeal('dessert') ? "yes<br>" : "no<br>";
+echo validMeal('lunch') ? "yes<br>" : "no<br>";
+*/
+
