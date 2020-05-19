beb43b7b6d7ff7fa6840f73f86abff5361fc714b
Arshdeep Singh <asingh69@mail.greenriver.edu>
2020-05-18 21:04:05 -0400

    updated content

--- index.php
+++ index.php
@@ -7,94 +7,33 @@ error_reporting(E_ALL);
 //Require the autoload file
 require_once("vendor/autoload.php");
 require_once("model/data-layer.php");
-require_once("model/validate.php");
-//require_once("classes/order.php");
 
 //Start a session AFTER requiring autoload.php
 session_start();
 
 //Instantiate the F3 Base class
 $f3 = Base::instance();
+$validator = new Validate();
+$controller = new Controller($f3, $validator);
 
 //Default route
 $f3->route('GET /', function() {
-    //echo '<h1>Welcome to my Food Page</h1>';
-
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['controller']->home();
 });
 
 //Order route
 $f3->route('GET|POST /order', function($f3) {
-
-    //If the form has been submitted
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        var_dump($_POST);
-        //["food"]=>"tacos" ["meal"]=>"lunch"
-
-        //Validate food
-        if (!validFood($_POST['food'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["food"]', "Invalid food item");
-        }
-        if (!validMeal($_POST['meal'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["meal"]', "Invalid meal.");
-        }
-        //Data is valid
-        if (empty($f3->get('errors'))) {
-
-            //Create an order object
-            $order = new FoodOrder();
-            $order->setFood($_POST['food']);
-            $order->setMeal($_POST['meal']);
-
-            //Store the object in the session array
-            $_SESSION['order'] = $order;
-
-            //Redirect to Order 2 page
-            $f3->reroute('order2');
-        }
-    }
-
-    $f3->set('meals', getMeals());
-    $f3->set('food', $_POST['food']);
-    $f3->set('selectedMeal', $_POST['meal']);
-    $view = new Template();
-    echo $view->render('views/order.html');
-
+    $GLOBALS['controller']->order();
 });
 
-//Order route
+//Order 2 route
 $f3->route('GET|POST /order2', function($f3) {
-
-    $conds = getCondiments();
-
-    //If the form has been submitted
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        //Add the data to the object in the session array
-        $_SESSION['order']->setCondiments($_POST['conds']);
-
-        //Redirect to summary page
-        $f3->reroute('summary');
-    }
-
-    $f3->set('conds', $conds);
-    $view = new Template();
-    echo $view->render('views/orderForm2.html');
+    $GLOBALS['controller']->order2();
 });
 
-//Breakfast route
+//summary route
 $f3->route('GET /summary', function() {
-    //echo '<h1>Thank you for your order!</h1>';
-
-    $view = new Template();
-    echo $view->render('views/summary.html');
-
-    session_destroy();
+    $GLOBALS['controller']->summary();
 });
 
 //Run F3
--- model/validate.php
+++ model/validate.php
@@ -1,40 +1,42 @@
 <?php
 
 //validate.php
+class Validate
+{
+    /*
+     * validfoods
+     */
 
-/*
- * validfoods
- */
+    function validFood($food)
+    {
 
-function validFood($food){
+        /*
+        if(!empty($food) && ctype_alpha($food)) {
+            return true;
+        } else {
+            return false;
+        }
+        */
+
+        $food = str_replace(' ', '', $food);
+        return !empty($food) && ctype_alpha($food);
+    }
+
+    /* Return a value indicating if meal is valid
+       Valid meals are breakfast, lunch and dinner
+       @param String $meal
+       @return boolean
+    */
+    function validMeal($meal)
+    {
+        $meals = getMeals();
+        return in_array($meal, $meals);
+    }
 
     /*
-    if(!empty($food) && ctype_alpha($food)) {
-        return true;
-    } else {
-        return false;
-    }
+    echo validMeal('breakfast') ? "yes<br>" : "no<br>";
+    echo validMeal('') ? "yes<br>" : "no<br>";
+    echo validMeal('dessert') ? "yes<br>" : "no<br>";
+    echo validMeal('lunch') ? "yes<br>" : "no<br>";
     */
-
-    $food = str_replace(' ', '', $food);
-    return !empty($food) && ctype_alpha($food);
 }
-
-/* Return a value indicating if meal is valid
-   Valid meals are breakfast, lunch and dinner
-   @param String $meal
-   @return boolean
-*/
-function validMeal($meal)
-{
-    $meals = getMeals();
-    return in_array($meal, $meals);
-}
-
-/*
-echo validMeal('breakfast') ? "yes<br>" : "no<br>";
-echo validMeal('') ? "yes<br>" : "no<br>";
-echo validMeal('dessert') ? "yes<br>" : "no<br>";
-echo validMeal('lunch') ? "yes<br>" : "no<br>";
-*/
-
